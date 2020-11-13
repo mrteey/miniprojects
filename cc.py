0b0cd8e2f340339f6c7ab22dfc64a4793808fb6d
Nasir Mustapha <teboy.com@gmail.com>
2020-11-13 15:44:46 +0100

    online offline converter

--- cc.py
+++ cc.py
@@ -3,6 +3,7 @@
 # run venv/Scripts/activate
 # run pip install PyQt5
 
+import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTextEdit, QComboBox
 
 app = QApplication([])
@@ -29,11 +30,11 @@ amount.setMaximumWidth(100)
 currencyFrom = QComboBox()
 currencyFrom.setMinimumHeight(30)
 currencyFrom.setMinimumWidth(100)
-currencyFrom.addItems(['NGN (₦)', 'USD ($)'])
+currencyFrom.addItems(['NGN (₦)', 'USD ($)', 'EURO (€)', 'POUND (£)', 'YEN (¥)'])
 currencyTo = QComboBox()
 currencyTo.setMinimumHeight(30)
 currencyTo.setMinimumWidth(100)
-currencyTo.addItems(['USD ($)', 'NGN (₦)' ])
+currencyTo.addItems(['NGN (₦)', 'USD ($)', 'EURO (€)', 'POUND (£)', 'YEN (¥)'])
 inputsLayout.addWidget(amount)
 inputsLayout.addWidget(currencyFrom)
 inputsLayout.addWidget(currencyTo)
@@ -48,23 +49,44 @@ container.addWidget(inputs)
 container.addWidget(button)
 container.addWidget(result)
 
-def _convert(amount, _from, to):
-    if '$' in _from:
-        if '₦' in to:
-            if amount.isdigit():
-                result.setText(f'${amount} equals to ₦{int(amount)*400}')
-            else:
-                result.setText('0')
-        else:
-            result.setText(amount)
-    else:
-        if '$' in to:
-            if amount.isdigit():
-                result.setText(f'₦{amount} equals to ${int(amount)/400}')
-            else:
-                result.setText('0')
-        else:
-            result.setText(amount)
-button.clicked.connect(lambda: _convert(amount.toPlainText(), currencyFrom.currentText(), currencyTo.currentText()))
+def convert_offline(amount, _from, to):
+    _from = _from.split('(')[-1][0]
+    to = to.split('(')[-1][0]
+    amount = int(amount) if amount.isdigit() else 1
+    conversions = {'$':
+                        {'₦':380, '€':140, '£':0.12, '$':1, '¥':0.034}, 
+                    '₦':
+                        {'$':0.0026, '€':0.00076, '£':0.00026, '₦':1, '¥':0.44},
+                    '£':
+                        {'$':112, '€':210, '₦':450, '£':1, '¥':0.74},
+                    '€':
+                        {'$':150, '£':15, '₦':550, '€':1, '¥':0.34},
+                    '¥':
+                        {'$':70, '£':30, '₦':89, '€':95, '¥':1}
+                    }
+    rate = conversions.get(_from).get(to)
+    value = rate*amount
+    result.setText(f'{_from}{amount} equals to {to}{value}')
+    return value
+
+def convert_online(amount, _from, to):
+    apikey = '20b9a28a6a37cdb1ff95'
+    _from = _from.split('(')[0]
+    to = to.split('(')[0]
+    amount = int(amount) if amount.isdigit() else 1
+    url = f'https://free.currconv.com/api/v7/convert?q={_from.strip()}_{to.strip()}&compact=ultra&apiKey={apikey}'
+    print(url)
+    r = requests.get(url)
+    rate = list(r.json().values())[0]
+    value = rate*amount
+    result.setText(f'{_from}{amount} equals to {to}{value}')
+
+def get_converter(amount, _from, to):
+    try:
+        return convert_online(amount, _from, to)
+    except:
+        return convert_offline(amount, _from, to)
+
+button.clicked.connect(lambda: get_converter(amount.toPlainText(), currencyFrom.currentText(), currencyTo.currentText()))
 mainWindow.show()
 app.exec_()
